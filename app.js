834c4411a38add16ca8ed023902ff2cce22c2b1d
hiteshreddy403 <89419246+hiteshreddy403@users.noreply.github.com>
2021-11-10 22:37:14 -0600

    commited

--- app.js
+++ app.js
@@ -3,15 +3,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var Costume = require("./models/costume"); 
+// database connection
+const connectionString =  
+process.env.MONGO_CON 
+mongoose = require('mongoose'); 
+mongoose.connect(connectionString,  
+{useNewUrlParser: true, 
+useUnifiedTopology: true});
+ 
+//Bind connection to error event
+var db = mongoose.connection;   
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){ 
+ console.log("Connection to DB succeeded")});  
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var TieRouter = require('./routes/Tie');
-var addmodsRouter = require('./routes/addmods');
-var selectorRouter = require('./routes/selector');
-
-
-
+//var university = require('./routes/university');
+var addmods = require('./routes/addmods');
+var selector = require('./routes/Selector');
+var resource = require('./routes/resource');
+var costume = require('./routes/costumes')
 
 var app = express();
 
@@ -27,11 +40,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/Tie', TieRouter);
-app.use('/addmods', addmodsRouter);
-app.use('/selector', selectorRouter);
-
-
+//app.use('/university',university);
+app.use('/addmods',addmods);
+app.use('/selector',selector);
+app.use('/resource',resource);
+app.use('/costume',costume)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -48,5 +61,38 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// We can seed the collection if needed on 
+async function recreateDB(){ 
+  // Delete everything 
+  await Costume.deleteMany();  
+  let instance1 = new 
+Costume({costume_type:"coat",  size:'large', 
+cost:25.4}); 
+  instance1.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("First object saved") 
+  }); 
+
+let instance2 = new 
+Costume({costume_type:"pant",  size:'medium', 
+cost:25.4}); 
+  instance2.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("second object saved") 
+  });
+  
+let instance3 = new 
+Costume({costume_type:"jeans",  size:'medium', 
+cost:25.4}); 
+  instance3.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("second object saved") 
+  }); 
+} 
+ 
+
+let reseed = true; 
+if (reseed) { recreateDB();} 
 
 module.exports = app;
--- routes/persis.js
+++ routes/persis.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('persis', { title: 'Search Results for persis' });
 });
 
 module.exports = router;
