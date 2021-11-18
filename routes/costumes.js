c5bc7358188dac5609349c3699098b5dea9d9ce1
hiteshreddy403 <89419246+hiteshreddy403@users.noreply.github.com>
2021-11-17 22:57:15 -0600

    updated code

--- routes/costumes.js
+++ routes/costumes.js
@@ -1,8 +1,21 @@
 var express = require('express'); 
-const costume_controlers= require('../controllers/costume'); 
+const costume_controllers= require('../controllers/costume'); 
+const detail_controller =  require('../controllers/constumedetail');
+const createcostume_controller = require('../controllers/createcostume');
+const costumeupdate_controller = require('../controllers/costumeupdate');
 var router = express.Router(); 
  
 /* GET costumes */ 
-router.get('/', costume_controlers.costume_view_all_Page ); 
+router.get('/', costume_controllers.costume_view_all_Page ); 
 module.exports = router; 
-router.get('/costumes/:id', costume_controlers.costume_detail);
+// GET request for one costume. 
+router.get('/costumes/:id', costume_controllers.costume_detail); 
+/* GET detail costume page */ 
+router.get('/detail', detail_controller.costume_view_one_Page); 
+/* GET create costume page */ 
+router.get('/create', createcostume_controller.costume_create_Page);
+/* GET create update page */ 
+router.get('/update', costumeupdate_controller.costume_update_Page); 
+/* GET create costume page */ 
+router.get('/delete', costume_controllers.costume_delete_Page); 
+ 
