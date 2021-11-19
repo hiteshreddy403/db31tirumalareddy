f4e3d531c0bfb1d77d7a2c33a6d6cfec83fb0c20
hiteshreddy403 <89419246+hiteshreddy403@users.noreply.github.com>
2021-11-18 22:03:24 -0600

    updated code

--- app.js
+++ app.js
@@ -3,31 +3,42 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var Costume = require("./models/costume");
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  } ));
+var Costume = require("./models/costume"); 
 // database connection
-const connectionString = "mongodb+srv://learningmongo:shashank1214@cluster0.p5q2s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-mongoose = require('mongoose');
-mongoose.connect(connectionString,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-
+const connectionString = process.env.MONGO_CON
+mongoose = require('mongoose'); 
+mongoose.connect(connectionString,  
+{useNewUrlParser: true, 
+useUnifiedTopology: true});
+ 
 //Bind connection to error event
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function () {
-  console.log("Connection to DB succeeded")
-});
+var db = mongoose.connection;   
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){ 
+ console.log("Connection to DB succeeded")});  
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-//var university = require('./routes/university');
+var university = require('./routes/university');
 var addmods = require('./routes/addmods');
-var selector = require('./routes/selector');
+var selector = require('./routes/Selector');
 var resource = require('./routes/resource');
-var costume = require('./routes/costumes');
-var persis = require('./routes/persis');
+var costume = require('./routes/costumes')
 
 var app = express();
 
@@ -39,24 +50,41 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/persis', persis);
-//app.use('/university',university);
-app.use('/addmods', addmods);
-app.use('/selector', selector);
-app.use('/resource', resource);
-app.use('/costume', costume);
+app.use('/university',university);
+app.use('/addmods',addmods);
+app.use('/selector',selector);
+app.use('/resource',resource);
+app.use('/costume',costume)
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account = require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
+
+ // catch 404 and forward to error handler
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -67,42 +95,36 @@ app.use(function (err, req, res, next) {
 });
 
 // We can seed the collection if needed on 
-async function recreateDB() {
+async function recreateDB(){ 
   // Delete everything 
-  await Costume.deleteMany();
-  let instance1 = new
-    Costume({
-      costume_type: "coat", size: 'large',
-      cost: 25.4
-    });
-  instance1.save(function (err, doc) {
-    if (err) return console.error(err);
-    console.log("First object saved")
+  //await Costume.deleteMany();  
+  let instance1 = new 
+Costume({costume_type:"coat",  size:'large', 
+cost:25.4}); 
+  instance1.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("First object saved") 
+  }); 
+
+let instance2 = new 
+Costume({costume_type:"pant",  size:'medium', 
+cost:25.4}); 
+  instance2.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("second object saved") 
   });
+  
+let instance3 = new 
+Costume({costume_type:"jeans",  size:'medium', 
+cost:25.4}); 
+  instance3.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("second object saved") 
+  }); 
+} 
+ 
 
-  let instance2 = new
-    Costume({
-      costume_type: "pant", size: 'medium',
-      cost: 25.4
-    });
-  instance2.save(function (err, doc) {
-    if (err) return console.error(err);
-    console.log("second object saved")
-  });
-
-  let instance3 = new
-    Costume({
-      costume_type: "jeans", size: 'medium',
-      cost: 25.4
-    });
-  instance3.save(function (err, doc) {
-    if (err) return console.error(err);
-    console.log("second object saved")
-  });
-}
-
-
-let reseed = true;
-if (reseed) { recreateDB(); }
+let reseed = true; 
+if (reseed) { recreateDB();} 
 
 module.exports = app;
