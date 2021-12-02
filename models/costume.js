3d9a57042066ef91058c79682e812ee2bb28422d
hiteshreddy403 <89419246+hiteshreddy403@users.noreply.github.com>
2021-12-01 23:14:11 -0600

    updated code

--- models/costume.js
+++ models/costume.js
@@ -1,15 +1,14 @@
-const mongoose = require("mongoose")
-const costumeSchema = mongoose.Schema({
-  costume_type: String,
-  size: String,
-  cost: Number
-}, {
-  writeConcern: {
-    w: 'majority',
-    j: true,
-    wtimeout: 1000
-  }
-})
-
-module.exports = mongoose.model("Costumes",
-  costumeSchema)
+const mongoose = require("mongoose") 
+const costumeSchema = mongoose.Schema({ 
+ costume_type: String, 
+ size: {type: String, maxlength: 6}, 
+ cost:{ type: Number, min: 5, max: 32 }
+},{
+    writeConcern: {
+      w: 'majority',
+      j: true,
+      wtimeout: 1000
+    }}) 
+ 
+module.exports = mongoose.model("Costumes", 
+costumeSchema) 
